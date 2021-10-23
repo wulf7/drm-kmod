wulf7/drm-kmod

commit 16fc7ed22b078abc3e5ff19fc3011f3630fddef5

Fix another one instance of "acquiring blockable sleep lock
with spinlock or critical section held" assertion on Intel Kaby Lake GPU
(Issue #9) with replacment of work_struct with its critical section-friendly
variant called irq_work.

The panic message is:

panic: acquiring blockable sleep lock with spinlock or critical section held (sleep mutex) linux_wq_exec @ /usr/src/sys/compat/linuxkpi/common/src/linux_work.c:105
cpuid = 3
time = 1617926020
KDB: stack backtrace:
db_trace_self_wrapper() at db_trace_self_wrapper+0x2b/frame 0xfffffe00da4e6010
vpanic() at vpanic+0x181/frame 0xfffffe00da4e6060
panic() at panic+0x43/frame 0xfffffe00da4e60c0
witness_checkorder() at witness_checkorder+0xf3e/frame 0xfffffe00da4e6280
__mtx_lock_flags() at __mtx_lock_flags+0x94/frame 0xfffffe00da4e62d0
linux_queue_work_on() at linux_queue_work_on+0x9a/frame 0xfffffe00da4e6310
intel_atomic_commit_ready() at intel_atomic_commit_ready+0x55/frame 0xfffffe00da4e6320
__i915_sw_fence_complete() at __i915_sw_fence_complete+0x188/frame 0xfffffe00da4e6370
dma_i915_sw_fence_wake_timer() at dma_i915_sw_fence_wake_timer+0x27/frame 0xfffffe00da4e6390
dma_fence_signal() at dma_fence_signal+0xc0/frame 0xfffffe00da4e63e0
dma_resv_add_shared_fence() at dma_resv_add_shared_fence+0xa6/frame 0xfffffe00da4e6430
i915_vma_move_to_active() at i915_vma_move_to_active+0x6b/frame 0xfffffe00da4e6470
i915_gem_do_execbuffer() at i915_gem_do_execbuffer+0x1414/frame 0xfffffe00da4e6680
i915_gem_execbuffer2_ioctl() at i915_gem_execbuffer2_ioctl+0x185/frame 0xfffffe00da4e66e0
drm_ioctl_kernel() at drm_ioctl_kernel+0x72/frame 0xfffffe00da4e6730
drm_ioctl() at drm_ioctl+0x2c8/frame 0xfffffe00da4e6820
linux_file_ioctl() at linux_file_ioctl+0x31d/frame 0xfffffe00da4e6880
kern_ioctl() at kern_ioctl+0x1ef/frame 0xfffffe00da4e68f0
sys_ioctl() at sys_ioctl+0x12a/frame 0xfffffe00da4e69c0
amd64_syscall() at amd64_syscall+0x124/frame 0xfffffe00da4e6af0
fast_syscall_common() at fast_syscall_common+0xf8/frame 0xfffffe00da4e6af0

drivers/gpu/drm/i915/display/intel_display.c
@@ -13889,7 +13889,11 @@ static void intel_atomic_helper_free_state(struct drm_i915_private *dev_priv)
 		drm_atomic_state_put(&state->base);
 }
 
+#ifdef __linux__
 static void intel_atomic_helper_free_state_worker(struct work_struct *work)
+#elif defined (__FreeBSD__)
+static void intel_atomic_helper_free_state_worker(struct irq_work *work)
+#endif
 {
 	struct drm_i915_private *dev_priv =
 		container_of(work, typeof(*dev_priv), atomic_helper.free_work);
@@ -14147,7 +14151,11 @@ intel_atomic_commit_ready(struct i915_sw_fence *fence,
 				&to_i915(state->base.dev)->atomic_helper;
 
 			if (llist_add(&state->freed, &helper->free_list))
+#ifdef __linux__
 				schedule_work(&helper->free_work);
+#elif defined (__FreeBSD__)
+				irq_work_queue(&helper->free_work);
+#endif
 			break;
 		}
 	}
@@ -16171,8 +16179,13 @@ int intel_modeset_init(struct drm_device *dev)
 	dev->mode_config.funcs = &intel_mode_funcs;
 
 	init_llist_head(&dev_priv->atomic_helper.free_list);
+#ifdef __linux__
 	INIT_WORK(&dev_priv->atomic_helper.free_work,
 		  intel_atomic_helper_free_state_worker);
+#elif defined (__FreeBSD__)
+	init_irq_work(&dev_priv->atomic_helper.free_work,
+		      intel_atomic_helper_free_state_worker);
+#endif
 
 	intel_init_quirks(dev_priv);
 
@@ -17113,7 +17126,15 @@ void intel_modeset_driver_remove(struct drm_device *dev)
 
 	flush_workqueue(dev_priv->modeset_wq);
 
+#ifdef __linux__
 	flush_work(&dev_priv->atomic_helper.free_work);
+#elif defined (__FreeBSD__)
+#if __FreeBSD_version < 1400025
+#define	irq_work_sync(irqw)	\
+	taskqueue_drain(linux_irq_work_tq, &(irqw)->irq_task)
+#endif
+	irq_work_sync(&dev_priv->atomic_helper.free_work);
+#endif
 	WARN_ON(!llist_empty(&dev_priv->atomic_helper.free_list));
 
 	/*

drivers/gpu/drm/i915/i915_drv.h
@@ -1499,7 +1499,13 @@ struct drm_i915_private {
 
 	struct intel_atomic_helper {
 		struct llist_head free_list;
+#ifdef __linux__
 		struct work_struct free_work;
+#elif defined (__FreeBSD__)
+		/* On FreeBSD this work is sporadically scheduled
+		 * within a critical section. */
+		struct irq_work free_work;
+#endif
 	} atomic_helper;
 
 	u16 orig_clock;

commit 8a5af4e20362967b5e15d209458ef7c35d9ddefb

Another one attempt to fix another one instance of "acquiring blockable
sleep lock with spinlock or critical section held" assertion on Intel
Kaby Lake GPU (Issue #9). Previous one 16fc7ed22b07 broke suspend/resume
on Whiskey Lake

The panic message is:

panic: acquiring blockable sleep lock with spinlock or critical section held (sleep mutex) linux_wq_exec @ /usr/src/sys/compat/linuxkpi/common/src/linux_work.c:105
cpuid = 3
time = 1617926020
KDB: stack backtrace:
db_trace_self_wrapper() at db_trace_self_wrapper+0x2b
vpanic() at vpanic+0x181
panic() at panic+0x43
witness_checkorder() at witness_checkorder+0xf3e
__mtx_lock_flags() at __mtx_lock_flags+0x94
linux_queue_work_on() at linux_queue_work_on+0x9a
intel_atomic_commit_ready() at intel_atomic_commit_ready+0x55
__i915_sw_fence_complete() at __i915_sw_fence_complete+0x188
dma_i915_sw_fence_wake_timer() at dma_i915_sw_fence_wake_timer+0x27
dma_fence_signal() at dma_fence_signal+0xc0
dma_resv_add_shared_fence() at dma_resv_add_shared_fence+0xa6
i915_vma_move_to_active() at i915_vma_move_to_active+0x6b
i915_gem_do_execbuffer() at i915_gem_do_execbuffer+0x1414
i915_gem_execbuffer2_ioctl() at i915_gem_execbuffer2_ioctl+0x185
drm_ioctl_kernel() at drm_ioctl_kernel+0x72
drm_ioctl() at drm_ioctl+0x2c8
linux_file_ioctl() at linux_file_ioctl+0x31d
kern_ioctl() at kern_ioctl+0x1ef
sys_ioctl() at sys_ioctl+0x12a
amd64_syscall() at amd64_syscall+0x124
fast_syscall_common() at fast_syscall_common+0xf8
--- syscall (54, FreeBSD ELF64, sys_ioctl), rip = 0x800a7188a, rsp = 0x7fffffffe2a8, rbp = 0x7fffffffe360 ---

drivers/gpu/drm/i915/display/intel_display.c
@@ -15512,6 +15512,9 @@ intel_atomic_commit_ready(struct i915_sw_fence *fence,
 				&to_i915(state->base.dev)->atomic_helper;
 
 			if (llist_add(&state->freed, &helper->free_list))
+#ifdef __FreeBSD__
+			    if (curthread->td_critnest == 0)
+#endif
 				schedule_work(&helper->free_work);
 			break;
 		}
@@ -18492,7 +18495,12 @@ void intel_modeset_driver_remove(struct drm_i915_private *i915)
 	flush_workqueue(i915->modeset_wq);
 
 	flush_work(&i915->atomic_helper.free_work);
+#ifdef __linux__
 	WARN_ON(!llist_empty(&i915->atomic_helper.free_list));
+#elif defined(__FreeBSD__)
+	/* schedule_work() can be skipped if called from critical section */
+	intel_atomic_helper_free_state(i915);
+#endif
 
 	/*
 	 * Interrupts and polling as the first thing to avoid creating havoc.